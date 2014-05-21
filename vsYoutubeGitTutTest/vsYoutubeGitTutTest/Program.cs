9fa4bf6bdd2c05a25f770eb8c419097eb26cdd05
samGit <sammyhumes@gmail.com>
2014-05-20 23:13:10 -0700

    vsYoutubeGit Inital Commit

---
+++ vsYoutubeGitTutTest/vsYoutubeGitTutTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vsYoutubeGitTutTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vsYoutbueGitClass ab = new vsYoutbueGitClass();
+            ab.NadaFunc();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ vsYoutubeGitTutTest/vsYoutubeGitTutTest/vsYoutbueGitClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vsYoutubeGitTutTest
+{
+    class vsYoutbueGitClass
+    {
+        int a;
+        public vsYoutbueGitClass() {
+            this.a = 10;
+
+        }
+        public void NadaFunc() {
+            Console.WriteLine("Just Nada");
+        }
+    }
+}
